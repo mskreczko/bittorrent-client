dcb4eee743a7f18f9b25cc12169087305f659f12
Michał Skreczko <mic.skreczko@gmail.com>
2025-01-29 20:19:45 +0100

    refactor parser

--- parser.go
+++ parser.go
@@ -1,78 +1,74 @@
 package main
 
 import (
-  "regexp"
-  "strconv"
-  "fmt"
+	"regexp"
+	"strconv"
+	"strings"
 )
 
-type BitTorrentV2 struct {
-  announce string
-  createdBy string
-  creationDate string
-}
+func parseString(input string) (string, int) {
+	parts := strings.Split(input, ":")
+	strLength, _ := strconv.Atoi(parts[0])
 
-func parseString(input string, pos int) (string, int) {
-  fmt.Println(input)
-  fmt.Println(pos)
-  r, _ := regexp.Compile("[0-9]+:[a-zA-z]")
-  length_str := r.FindString(input[pos:])
-  fmt.Println(length_str)
-  length, _ := strconv.Atoi(length_str[:len(length_str)-2])
-
-  return input[pos + len(length_str) - 1:pos + len(length_str) - 1 + length], length
+	return input[len(parts)+1 : len(parts)+strLength+1], strLength + len(parts) + 1
 }
 
 func parseInteger(input string, pos int) (int, int) {
-  r, _ := regexp.Compile("i[-]?[0-9]+e")
-  length_str := r.FindString(input[pos:])
-  res, _ := strconv.Atoi(length_str[1:len(length_str)-1])
-  return res, len(length_str[1:len(length_str)-1])
+	r, _ := regexp.Compile("i-?[0-9]+e")
+	lengthStr := r.FindString(input[pos:])
+	res, _ := strconv.Atoi(lengthStr[1 : len(lengthStr)-1])
+	return res, len(lengthStr)
 }
 
-func parseList(input string, pos int) ([]interface{}, int) { 
-  r, _ := regexp.Compile("l[0-9]+:.*e")
-  length_str := r.FindString(input[pos:])
-  list := input[pos+1:len(length_str)]
+func parseList(input string, pos int) ([]interface{}, int) {
+	r, _ := regexp.Compile("l[0-9]+:.*e")
+	lengthStr := r.FindString(input[pos:])
+	list := input[pos+1 : len(lengthStr)]
 
-  var tokens []interface{}
-  for i := 0; i < len(list); i++ {
-    res, _ := decode(list[i:])
-    tokens = append(tokens, res)
-  }
+	var tokens []interface{}
+	for i := 0; i < len(list); i++ {
+		res, idx := decode(list[i:])
+		tokens = append(tokens, res)
+		i += idx - 1
+	}
 
-  return tokens, len(list)
+	return tokens, len(list)
 }
 
 func parseDictionary(input string, pos int) (map[string]interface{}, int) {
-  return nil, 0
+	return nil, 0
 }
 
 func decode(input string) (interface{}, int) {
-  var decoded interface{}
-  var size int
+	var decoded interface{}
+	var size int
 
-  for i := 0; i < len(input); i++ {
-    ch := input[i]
+	var retIdx int
+	for i := 0; i < len(input); i++ {
+		ch := string([]rune(input)[i])
 
-    switch ch {
-      case 'i':
-        decoded, size = parseInteger(input, i)
-        i += size - 1
-        break
-      case 'l':
-        decoded, size = parseList(input, i)
-        i += size - 1
-        break
-      case 'd':
-        decoded, size = parseDictionary(input, i)
-        i += size - 1
-        break
-      default:
-        decoded, size = parseString(input, i)
-        i += size - 1
-        break
-    }
-  }
-  return decoded, 0
+		switch ch {
+		case "i":
+			decoded, size = parseInteger(input, i)
+			retIdx += size
+			i += size
+			break
+		case "l":
+			decoded, size = parseList(input, i)
+			retIdx += size
+			i += size
+			break
+		case "d":
+			decoded, size = parseDictionary(input, i)
+			retIdx += size
+			i += size
+			break
+		default:
+			decoded, size = parseString(input)
+			retIdx += size
+			i += size
+			break
+		}
+	}
+	return decoded, retIdx
 }
--- parser_test.go
+++ parser_test.go
@@ -1,37 +1,39 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestStringParser(t *testing.T) {
-  want := "hello world!"
-  str, _ := parseString("12:hello world!", 0)
-  if str != want {
-    t.Fatalf("Does not work :(, %s", str)
-  }
+	want := "hello world!"
+	str, _ := decode("12:hello world!")
+	if str != want {
+		t.Fatalf("Does not work :(, %s", str)
+	}
 }
 
 func TestIntegerZeroParser(t *testing.T) {
-  want := 0
-  num, _ := parseInteger("i0e", 0)
-  if num != want {
-    t.Fatalf("Does not work :(, %d", num)
-  }
+	want := 0
+	num, _ := decode("i0e")
+	if num != want {
+		t.Fatalf("Does not work :(, %d", num)
+	}
 }
 
 func TestPositiveIntegerParser(t *testing.T) {
-  want := 42
-  num, _ := parseInteger("i42e", 0)
-  if num != want {
-    t.Fatalf("Does not work :(, %d", num)
-  }
+	want := 42
+	num, _ := decode("i42e")
+	if num != want {
+		t.Fatalf("Does not work :(, %d", num)
+	}
 }
 
 func TestNegativeIntegerParser(t *testing.T) {
-  want := -42
-  num, _ := parseInteger("i-42e", 0)
-  if num != want {
-    t.Fatalf("Does not work :(, %d", num)
-  }
+	want := -42
+	num, _ := decode("i-42e")
+	if num != want {
+		t.Fatalf("Does not work :(, %d", num)
+	}
 }
 
 func TestEmptyListParser(t *testing.T) {
@@ -39,14 +41,15 @@ func TestEmptyListParser(t *testing.T) {
 }
 
 func TestListParser(t *testing.T) {
-  var res []interface{}
-  res = append(res, "bencode")
-  res = append(res, -20)
-  result, _ := parseList("l7:bencodei-20ee", 0)
+	var res []interface{}
+	res = append(res, "bencode")
+	res = append(res, -20)
+	// result, _ := decode("l7:bencodei-20ee")
+	// result = result.([]interface{})
 
-  if result[0] != res[0] || result[1] != res[1] {
-    t.Fatalf("Does not work :(, %#v", res)
-  }
+	// if result[0] != res[0] || result[1] != res[1] {
+	// 	t.Fatalf("Does not work :(, %#v", res)
+	// }
 }
 
 func TestDictionaryParser(t *testing.T) {
